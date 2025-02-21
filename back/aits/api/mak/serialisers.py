c2172577d81a2eba20d229c7d2c266475746a60b
mmark6 <mmukisa64@gmail.com>
2025-02-21 06:07:21 +0300

    Delete back/aits/api directory

--- back/aits/api/mak/serialisers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from . models import *
-
-
-class ProductSerialiser(serializers.ModelSerializer):
-    class Meta:
-        model = Products
-        fields = "__all__"

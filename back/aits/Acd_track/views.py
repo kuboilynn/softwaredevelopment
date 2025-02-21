13e52582fa10d19896cdc6caeb6eea19941012b9
mmark6 <mmukisa64@gmail.com>
2025-02-21 06:09:52 +0300

    Update views.py

--- back/aits/Acd_track/views.py
+++ back/aits/Acd_track/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 
+
 # Create your views here.
 @api_view(['GET'])
 def homeview(request):

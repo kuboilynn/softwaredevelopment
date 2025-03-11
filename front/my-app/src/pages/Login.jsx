ee6ab4da16aec0bc2bd7a25e1c77841b1398d004
stuartmutebi <stuartmutebi07@gmail.com>
2025-03-11 11:44:59 +0300

    Modified the Login page with some css

--- front/my-app/src/pages/Login.jsx
+++ front/my-app/src/pages/Login.jsx
@@ -1,9 +1,10 @@
 import Input from "../UI/input";
 import { useState } from "react";
 import '../index.css';
-import makerere from "../assets/makerere_university.jpg"
+import makerere from "../assets/makererelogo.png"
 import Img from "../UI/Img"
 import Button from "../UI/Button"
+import './styles/Login.css';
 
 function Login() {
 
@@ -16,13 +17,12 @@ function Login() {
   }
   return (
     <div className="Login">
-      <h3> Login</h3>
+      <h3>Login</h3>
       <Img src={makerere} alt="Makerere Logo"/>
       <Input type="text" value={email} onchange={enteremail}></Input><br/>
       <Input type="text" value={email} onchange={enteremail}></Input><br/>
       <Button>Login</Button>
-      <p>Don't have an account? Signup</p>
-      
+      <p>Don't have an account? Signup</p> 
     </div>
   )
 }

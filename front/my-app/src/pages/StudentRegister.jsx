bc753588053cce75c276bab9981f3a006f1ca56f
kuboilynn <kuboilynn4@gmail.com>
2025-03-11 03:39:39 -0700

    added placeholders to input

--- front/my-app/src/pages/StudentRegister.jsx
+++ front/my-app/src/pages/StudentRegister.jsx
@@ -1,9 +1,18 @@
 import Button from "../UI/Button"
 import Input from "../UI/input";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function StudentRegister(){
   const navigate=useNavigate()
+  const[name,setname]=useState()
+  function entername(event){
+    if(setname===""){
+      return"*required"
+
+    }
+    setname(event.target.value);
+  }
 
   function gotoStudentHome(){
     navigate("./")
@@ -12,11 +21,12 @@ function StudentRegister(){
   return(
     <div>
       <h3>Register: Student</h3>
-      <Input type="text" ></Input><br/>
-      <Input type="text" ></Input><br/>
-      <Input type="text" ></Input><br/>
-      <Input type="text" ></Input><br/>
-      <Input type="text" ></Input><br/>
+      <Input type="text" value={name} onChange={entername} placeholder="Name"></Input><br/>
+      <Input type="text" placeholder="Email"></Input><br/>
+      <Input type="text" placeholder="Reg No"></Input><br/>
+      <Input type="text"  placeholder="Student No"></Input><br/>
+      <Input type="text" placeholder="password"></Input><br/>
+      <Input type="text" placeholder="confirm password"></Input><br/>
       <Button onClick={gotoStudentHome}>Signup</Button>
     </div>
   )

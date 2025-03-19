cd0fff92156d78893b4032d193a07cd0702e17e6
stuartmutebi <stuartmutebi07@gmail.com>
2025-03-19 17:57:59 +0300

    The issue card contains all the complaints from Students and here the lecturer will be able to view them

--- front/my-app/src/LecturerDashBoard/IssueCard.jsx
+++ front/my-app/src/LecturerDashBoard/IssueCard.jsx
@@ -1,10 +1,49 @@
 import React from "react";
 import '../pages/styles/IssueCard.css';
 
+const IssueCard = ({issue}) =>{
+    return(
+        <table className="Issue-card-table">
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Student</th>
+                    <th>Status</th>
+                    <th>Description</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody> 
+                <tr className="issue-card-row">
+            <td>{issue?.title||'Issue Title'}</td>
+
+            <td><strong>Student:</strong>{issue?.students||'Student Name'}</td>
+            <td><strong>Status:</strong>{issue?.status||'Issue status'}</td>
+            <td>{issue?.description||'Issue Description goes here....'}</td>
+            <td>
+            <button className="resolve-button">Resolve</button>
+            <button className="communication-button">Communication</button>
+            </td>
+            </tr>
+            </tbody>
+            
+
+        </table>
+    );
+}
+
+export default IssueCard;
+
+
+
+/*import React from "react";
+import '../pages/styles/IssueCard.css';
+
 const IssueCard = ({issue})=>{
     if(!issue) {
         return <div>Loading.....</div>
     }
+    console.log(issue);
     return(
         <div className="Issue-card">
             <h3>{issue.title}</h3>
@@ -16,4 +55,4 @@ const IssueCard = ({issue})=>{
         </div>
     )
 }
-export default IssueCard;
+export default IssueCard;*/

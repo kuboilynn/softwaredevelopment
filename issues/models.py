20985d03316d04ef453e4d3ad86b10debdc3dbd5
iam-joze <arobajoseph7@gmail.com>
2025-03-19 11:23:10 +0300

    notifications

--- issues/models.py
+++ issues/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.core.mail import send_mail
+from django.conf import settings
 
 # Create your models here.
 
@@ -23,6 +24,7 @@ class Issue(models.Model):
             ("can_resolve_issues", "Can resolve issues"),
             ("can_assign_issues", "Can assign issues"),
         ]
+    
 
     student = models.ForeignKey(User, on_delete=models.CASCADE, related_name='student_issues')
     lecturer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='lecturer_issues')
@@ -36,3 +38,16 @@ class Issue(models.Model):
     def __str__(self):
         return f'{self.issue_type} - {self.student.username}'
     
+    def save(self, *args, **kwargs):
+        if self.pk: #check if the isuue is being updated
+            old_issue = Issue.objects.get(pk=self.pk)
+            if old_issue.status != self.status:
+                send_mail(
+                    f'Issue Status Update: {self.issue_type}',
+                    f'Your issue status has been updated to {self.status}',
+                    settings.EMAIL_HOST_USER,
+                    [self.student.email],
+                    fail_silently=False,
+                )
+        super().save(*args, **kwargs)
+        

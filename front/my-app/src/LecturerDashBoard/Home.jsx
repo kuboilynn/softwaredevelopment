5acd7eb8c5876377d2b0620db85fc24907ce54c7
stuartmutebi <stuartmutebi07@gmail.com>
2025-03-20 15:41:46 +0300

    Created the Home.jsx where i created a banner

--- front/my-app/src/LecturerDashBoard/Home.jsx
+++ front/my-app/src/LecturerDashBoard/Home.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import '../pages/styles/Home.css';
+import image1 from '../assets/images/image1.png';
 
 const Home=()=>{
+    
     return(
-        <div>
-            <h2>Home</h2>
-            <p>Home</p>
+        <div className="home">
+            <div className="banner">
+                <img src={image1} alt="Banner" className="banner-image"/>
+                <h2>Welcome to your DashBoard</h2>
+                <p>Your space to manage everything Effortlessly</p>
+            </div>
         </div>
     );
 };

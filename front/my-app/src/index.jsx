ef7ace4811ee6dd0fd26497abbf89da60c550048
kuboilynn <kuboilynn4@gmail.com>
2025-03-26 03:16:17 -0700

    added more paths in index

--- front/my-app/src/index.jsx
+++ front/my-app/src/index.jsx
@@ -5,8 +5,11 @@ import App from './App';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import StudentRegister from './pages/studentpages/StudentRegister';
-import StudentHome from './pages/studentpages/StudentHome';
+import StudentRegister from './pages/StudentRegister';
+import StudentHome from './pages/StudentHome';
+import StudentIssues from './pages/StudentIssues';
+import FileIssue from './pages/FileIssue';
+import Profile from './pages/Profile';
 
 
 const root =ReactDOM.createRoot(document.getElementById('root'));root.render(
@@ -18,6 +21,9 @@ const root =ReactDOM.createRoot(document.getElementById('root'));root.render(
       <Route path="/Login" element={<Login/>}/>
       <Route path="/StudentRegister" element={<StudentRegister/>}/>
       <Route path="/StudentHome" element={<StudentHome/>}/>
+      <Route path="/StudentIssues" element={<StudentIssues/>}/>
+      <Route path="/FileIssue" element={<FileIssue/>}/>
+      <Route path="/Profile" element={<Profile/>}/>
     </Routes>
     </BrowserRouter>
     

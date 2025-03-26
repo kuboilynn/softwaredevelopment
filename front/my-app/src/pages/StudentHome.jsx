6fd71bcdf143708560d25662c0e97c01e0559904
kuboilynn <kuboilynn4@gmail.com>
2025-03-26 03:13:06 -0700

    chnges to student home

---
+++ front/my-app/src/pages/StudentHome.jsx
@@ -0,0 +1,20 @@
+import DashboardStudent from "./DashboardStudent"
+import './styles/StudentHome.css';
+
+
+function StudentHome(){
+  return(<div>
+          <DashboardStudent/>
+          <div className="content-home">
+            <div id="boxes">
+              <div>No more stress
+               
+              </div>
+              <div>2</div>
+              <div>3</div>
+            </div>
+            
+            </div>
+  </div>)
+}
+export default StudentHome

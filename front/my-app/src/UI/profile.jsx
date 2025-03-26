162b9d5748804809addaaf55b86cb243c21154ac
kuboilynn <kuboilynn4@gmail.com>
2025-03-26 03:15:10 -0700

    made changes to profile

---
+++ front/my-app/src/UI/profile.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+const ProfilePic=styled.img`
+  width:200px;
+  height:200px;
+  padding-bottom:50px;
+  padding-top:20px;
+  border-radius:200px;
+`;
+
+export default ProfilePic;

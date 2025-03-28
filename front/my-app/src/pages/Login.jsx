74fcb3fdcfddaed860733755e86b06127d5d3b23
stuartmutebi <stuartmutebi07@gmail.com>
2025-03-28 19:09:36 +0300

    Reorganised the fields for the password and registration no

--- front/my-app/src/pages/Login.jsx
+++ front/my-app/src/pages/Login.jsx
@@ -3,31 +3,66 @@ import { useState } from "react";
 import '../index.css';
 import makerere from "../assets/makererelogo.png"
 import Img from "../UI/Img"
-import Button from "../UI/Button"
 import './styles/Login.css';
 
+
 function Login() {
+  const[regno, setRegno]=useState("");
+  const [password, setPassword]= useState("");
+  const[showError, setShowError]=useState(false);
 
-  const[regno,setregno]=useState();
-
-  function enterregno(event){
-    if (setregno===""){
-      return "reg no required"
-    }
-    setregno(event.target.value);
-
-
+  function handleRegnoChange(event){
+    setRegno(event.target.value);
+    setShowError(false);
   }
+
+  function handlePasswordChange(event){
+    setPassword(event.target.value);
+  }
+
+  function handleLogin(){
+    if(regno === ""){
+      setShowError(true);
+    } else {
+      setShowError(false);
+      console.log("Login attempted with:", {regno,password});
+
+    }
+  }
+
   return (
     <div className="Login">
-      <h3>Login</h3>
-      <Img src={makerere} alt="Makerere Logo"/>
-      <Input type="text" value={regno} onchange={enterregno}placeholder="Student no/Reg"></Input><br/>
-      <Input type="text" value={regno} onchange={enterregno}placeholder="Name"></Input><br/>
-      <Button>Login</Button>
-      <p>Don't have an account? Signup</p> 
+      <Img src={makerere} alt="Makerere Logo" className="logo"/>
+      <p className="access">Access Your Academic Dashboard</p>
+
+      <div className="input-group">
+      <Input 
+      type="text" 
+      value={regno} 
+      onChange={handleRegnoChange}
+      placeholder="Student No/Reg" 
+      className="input-field"
+      />
+      {showError && <span className="error">Reg no required</span>}
+      </div>
+
+      <div className="input-group">
+        <input 
+        type="password"
+        value={password}
+        onChange={handlePasswordChange}
+        placeholder="Password"
+        className="input-field"
+        />
+      </div>
+      <button className="login-btn" 
+            onClick={handleLogin}>
+        Login
+      </button>
+
+      <p className="signup-link">Don't have an account? <a href="/Register">Signup</a></p> 
     </div>
-  )
+  );
 }
 
 export default Login

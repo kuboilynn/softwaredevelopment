3a2613a922218c2c2b732840bf35520d92ea3df2
kuboilynn <kuboilynn4@gmail.com>
2025-04-02 01:40:37 -0700

    changed issue filing and home messages

--- front/my-app/src/pages/StudentHome.jsx
+++ front/my-app/src/pages/StudentHome.jsx
@@ -12,12 +12,15 @@ function StudentHome(){
   return(<div>
           <DashboardStudent/>
           <div className="content-home">
-            <div>
+            <div className="home-message">
             welcome to easier problem solving
             </div>
-           
-            
+            <p className="message2">
+            Looking for a system to make issue filing and follow up easier look no further.
+            This is a system developed by fellow students that understand the dillema
+            </p>
             </div>
+            
   </div>)
 };
 export default StudentHome

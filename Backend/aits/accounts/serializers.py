7ec5ad64e1c34f7b9dc98a9d9b56bae21d44878f
mmark6 <mmukisa64@gmail.com>
2025-04-02 21:25:04 +0300

    made the serialisers

--- Backend/aits/accounts/serializers.py
+++ Backend/aits/accounts/serializers.py
@@ -11,6 +11,8 @@ class UserSerialiser(serializers.ModelSerializer):
         fields = ["id", "username", "email", "first_name", "password", "last_name", "last_login", "date_joined"]
         extra_kwargs = {'password': {'write_only': True}}
 
+        
+
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user

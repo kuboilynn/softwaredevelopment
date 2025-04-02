ddc69ab3683ff9c083f49e3dd332e366bc4be15e
mmark6 <mmukisa64@gmail.com>
2025-04-02 22:06:20 +0300

    in notification  models

--- Backend/aits/Acd_track/models.py
+++ Backend/aits/Acd_track/models.py
@@ -15,6 +15,6 @@ class Submission(models.Model):
       
 
 class Notification(models.Model):
-    #user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)

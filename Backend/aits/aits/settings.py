22bac4881e9647ff131466d3a3b31be00528c770
mmark6 <mmukisa64@gmail.com>
2025-04-02 21:14:39 +0300

    made migrations for some models

--- Backend/aits/aits/settings.py
+++ Backend/aits/aits/settings.py
@@ -46,8 +46,8 @@ INSTALLED_APPS = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080",
-    "http://127.0.0.1:9000",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
 MIDDLEWARE = [

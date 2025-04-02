ff627171bbb0d58f85a1b80e13ca2cb20f7aff58
mmark6 <mmukisa64@gmail.com>
2025-04-02 22:28:51 +0300

    removed

--- Backend/aits/accounts/urls.py
+++ Backend/aits/accounts/urls.py
@@ -9,9 +9,8 @@ urlpatterns = [
     path("activate/<str:uidb64>/<str:token>", activate_account, name="activate_account"),
     path("token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
     
-
     path("profile", user_profile, name="profile"),
     path("request-password-reset", request_password_reset, name="request_password_reset"),
     path('reset-password/<str:uidb64>/<str:token>', reset_password, name='reset-password'),
-    
+
 ]

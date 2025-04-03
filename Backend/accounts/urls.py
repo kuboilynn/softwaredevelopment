ffd4f85ce78fe18f41e7a19c41863eef333bb9a5
mmark6 <mmukisa64@gmail.com>
2025-04-03 17:35:46 +0300

    updated the urls in accounts

--- Backend/accounts/urls.py
+++ Backend/accounts/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import register, login, user_profile, activate_account, request_password_reset, reset_password
+from .views import register, login, user_profile, activate_account, request_password_reset, reset_password, logout_view, logout_all_view
 
 
 urlpatterns = [
     path("register", register, name="register"),
     path("login", login, name="login"),
+    path("logout", logout_view, name="logout"),
+    path("logout-all", logout_all_view, name="logout_all"),
     path("activate/<str:uidb64>/<str:token>", activate_account, name="activate_account"),
     path("token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
     

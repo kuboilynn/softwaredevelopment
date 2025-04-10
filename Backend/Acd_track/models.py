b577fc2fe9067c372245d87ece674045dd76ec44
mmark6 <mmukisa64@gmail.com>
2025-04-10 16:12:57 +0300

    Update models.py

--- Backend/Acd_track/models.py
+++ Backend/Acd_track/models.py
@@ -15,6 +15,7 @@ class Submission(models.Model):
       
 
 class Notification(models.Model):
+    
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)

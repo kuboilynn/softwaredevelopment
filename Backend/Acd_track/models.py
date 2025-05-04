c1986167769a4dbb9cfe6d331fbc33372e0e2a7e
mmark6 <mmukisa64@gmail.com>
2025-05-04 07:08:37 +0300

    added a submission date

--- Backend/Acd_track/models.py
+++ Backend/Acd_track/models.py
@@ -10,6 +10,7 @@ class Submission(models.Model):
     description = models.TextField()
     status = models.CharField(max_length=100,default="pending")
     submission_date = models.DateTimeField(auto_now_add=True)
+    
 
     def __str__(self):
         return f"{self.student}'s submission for {self.student.get_full_name()}"

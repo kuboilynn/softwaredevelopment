28fdafadf01a76db8bab42f6da3ee9e0f6a038c1
mmark6 <mmukisa64@gmail.com>
2025-05-04 07:11:32 +0300

    Update serializers.py

--- Backend/accounts/serializers.py
+++ Backend/accounts/serializers.py
@@ -10,7 +10,6 @@ class UserSerialiser(serializers.ModelSerializer):
         model = User
         fields = ["id", "username", "email", "first_name", "password", "last_name", "last_login", "date_joined"]
         extra_kwargs = {'password': {'write_only': True}}
-
     
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)

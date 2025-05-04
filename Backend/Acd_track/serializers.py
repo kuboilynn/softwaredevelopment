03206a48bcb6b1b1119084b717c5eb3e61f73ae2
mmark6 <mmukisa64@gmail.com>
2025-05-04 07:04:54 +0300

    added a field in the serailsiers

--- Backend/Acd_track/serializers.py
+++ Backend/Acd_track/serializers.py
@@ -8,6 +8,7 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'username', 'first_name', 'last_name', 'email', 'is_staff']
         
+        
 
 class SubmissionsSerializer(serializers.ModelSerializer):
     class Meta:

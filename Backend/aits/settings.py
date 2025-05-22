5f3c4bbd04dbfa906c6336d7aa509c15955eb39c
kuboilynn <kuboilynn4@gmail.com>
2025-05-22 15:56:21 +0300

    deploying

--- Backend/aits/settings.py
+++ Backend/aits/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 from datetime import timedelta
 
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-7446ccdphapyd*++4$3pnj@=fvm^nc@vn)xi8r+t4p0au=vj2e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["your-heroku-app-name.herokuapp.com', 'localhost', '127.0.0.1']"]
 
 
 # Application definition
@@ -102,7 +102,7 @@ ROOT_URLCONF = 'aits.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'build')],  
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,6 +114,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'aits.wsgi.application'
 
@@ -181,9 +182,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 
-STATIC_URL = 'static/'
-STATIC_ROOT = 'static'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, '../frontend/build/static'),
+]
 MEDIA_URL = 'media/'
 MEDIA_ROOT = 'media'
 
--- Backend/aits/urls.py
+++ Backend/aits/urls.py
@@ -14,6 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.urls import path, re_path
+from django.views.generic import TemplateView
+
 from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
@@ -25,6 +29,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Acd_track.urls')),
     path('accounts/', include('accounts.urls')),
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
     #path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]+static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)+static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
  

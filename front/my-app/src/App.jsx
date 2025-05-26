c6e7eb97cf97dc77a392fdbfd8b788ec7b3db3db
mmark6 <mmukisa64@gmail.com>
2025-05-26 17:37:38 +0300

    Update App.jsx

--- front/my-app/src/App.jsx
+++ front/my-app/src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import RegisterandLogin from "./pages/RegisterandLogin";
-import API from "./pages/API";
+
 function App() {
   return(
      <> <div>
      <RegisterandLogin/>
-     <API/>
+   
    </div></>
    
   );
